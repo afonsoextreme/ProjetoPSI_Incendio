a14ab03570d9a7a3b5b7ee377c899d537f72bd33
Manso8 <146339358+Manso8@users.noreply.github.com>
2024-10-09 16:06:29 +0100

    teste2

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
-google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.load('current', { packages: ['corechart', 'bar'] });
 google.charts.setOnLoadCallback(drawBasic);
 
 function drawBasic() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Mês');
+    data.addColumn('number', 'Número de Incêndios');
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('timeofday', 'Time of Day');
-      data.addColumn('number', 'Motivation Level');
+    // Dados de incêndios por mês
+    data.addRows([
+        ['Janeiro', 200],
+        ['Fevereiro', 250],
+        ['Março', 300],
+        ['Abril', 450],
+        ['Maio', 500],
+        ['Junho', 600],
+        ['Julho', 800],
+        ['Agosto', 1671],
+        ['Setembro', 800]
+    ]);
 
-      data.addRows([
-        [{v: [8, 0, 0], f: '8 am'}, 1],
-        [{v: [9, 0, 0], f: '9 am'}, 2],
-        [{v: [10, 0, 0], f:'10 am'}, 3],
-        [{v: [11, 0, 0], f: '11 am'}, 4],
-        [{v: [12, 0, 0], f: '12 pm'}, 5],
-        [{v: [13, 0, 0], f: '1 pm'}, 6],
-        [{v: [14, 0, 0], f: '2 pm'}, 7],
-        [{v: [15, 0, 0], f: '3 pm'}, 8],
-        [{v: [16, 0, 0], f: '4 pm'}, 9],
-        [{v: [17, 0, 0], f: '5 pm'}, 10],
-      ]);
-
-      var options = {
-        title: 'Motivation Level Throughout the Day',
+    var options = {
+        title: 'Número de Incêndios por Mês',
         hAxis: {
-          title: 'Time of Day',
-          format: 'h:mm a',
-          viewWindow: {
-            min: [7, 30, 0],
-            max: [17, 30, 0]
-          }
+            title: 'Número de Incêndios',
+            minValue: 0
         },
         vAxis: {
-          title: 'Rating (scale of 1-10)'
-        }
-      };
+            title: 'Mês'
+        },
+        colors: ['#FF69B4'],  // Cor rosa
+        legend: { position: 'none' }
+    };
 
-      var chart = new google.visualization.ColumnChart(
-        document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
